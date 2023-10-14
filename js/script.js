f7cc2116b369cff5d215385d03116a5e8fce9f4d
Karthik Krishnan K K <110273701+KarthikKKrishnan@users.noreply.github.com>
2023-10-14 13:03:40 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 // toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
 
 // scroll sections
 let sections = document.querySelectorAll('section')
@@ -6,7 +13,7 @@ let navLinks = document.querySelectorAll('header nav a')
 
 window.onscroll = () => {
     sections.forEach(sec => {
-        let top =window.scrollY;
+        let top = window.scrollY;
         let offset = sec.offsetTop - 100;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
@@ -15,12 +22,19 @@ window.onscroll = () => {
            // active navbar links
            navLinks.forEach(links => {
             links.classList.remove('active');
-            document.querySelector('header nav a[href*='+ id + ']').classList.add('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
            });
         }
     });
     //sticky header
     let header = document.querySelector('header');
     header.classList.toggle('sticky' , window.scrollY > 100); 
+    
+
+    // remove toggle icon and navbar when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 }
+
+
 
